e8d498c83bd22212594cb22128c30996edd14fa3
Yun Ting Hung <s9923554@gmail.com>
2018-06-24 17:11:21 +0800

    add admin/user index page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,16 @@
 class Admin::UsersController < ApplicationController
+before_action :authenticate_user!
+before_action :authenticate_admin
+
+  def index
+    @users = User.all
+  end
+
+private 
+  def authenticate_admin
+      unless current_user.admin?
+        flash[:alert] = "Not allow!"
+        redirect_to root_path
+      end
+  end
 end
