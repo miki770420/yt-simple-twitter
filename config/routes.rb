457eb81d30ff4e47a714dc3a2f2fd89a8f5f4111
Yun Ting Hung <s9923554@gmail.com>
2018-06-27 20:32:56 +0800

    add reply routes for index and create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :tweets, only: [:index, :show, :create]
+  resources :tweets, only: [:index, :show, :create] do
+    resources :replies, only: [:index, :create]
+  end
+
   resources :users, only: [:edit, :update, :show]
 
   root "tweets#index"
